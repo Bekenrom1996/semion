412100d7e60146a87cca658bae9ec6016d646a30
Bekenrom1996 <106440187+Bekenrom1996@users.noreply.github.com>
2024-05-07 19:17:20 -0700

    Add files via upload

---
+++ me.js
@@ -0,0 +1,29 @@
+
+
+function showjc(){
+    document.getElementById("jc3").style.display= "flex";
+    document.getElementById("jc3").onclick=function(){
+        document.getElementById("jc3").style.display= "none";
+    };
+}
+
+function showjc1(){
+    document.getElementById("sdsol").style.display = "flex";
+    document.getElementById("sdsol").onclick=function(){
+        document.getElementById("sdsol").style.display= "none";
+    };
+}
+
+function showjc2(){
+    document.getElementById("tdk").style.display = "flex";
+    document.getElementById("tdk").onclick=function(){
+        document.getElementById("tdk").style.display= "none";
+    };
+}
+
+function showjc3(){
+    document.getElementById("jc4").style.display = "flex";
+    document.getElementById("jc4").onclick=function(){
+        document.getElementById("jc4").style.display= "none";
+    };
+}
